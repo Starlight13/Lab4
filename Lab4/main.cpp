f3bf98d37a586b41fb3c624b397f1adcd157bb48
dsadas asdasd <olgarom@macbook-pro.local>
2019-05-15 09:58:41 +0300

    Interface

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,15 +1,47 @@
-//
-//  main.cpp
-//  Lab4
-//
-//  Created by dsadas asdasd on 5/15/19.
-//  Copyright © 2019 dsadas asdasd. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+void compress(int argc,const char *argv[]){
+    cout << "compress";
+}
+void decompress(int argc, const char *argv[]){
+    cout << "decompress";
+}
+
+class Interface{
+public:
+    Interface(int argc, const char* argv[]){
+        if (!(argc > 1)) {
+            cout << "Not enough arguments";
+            return;
+        }
+        else if(!strncmp(argv[1], "--compress", 10)){
+            if(argc>=4)
+                compress(argc, argv);
+            else{
+                cout << "Enter at 1 output file and at least 1 file to compress";
+                return;
+            }
+        }
+        else if(!strncmp(argv[1], "--decompress", 12)){
+            if(argc==3)
+                decompress(argc, argv);
+            else{
+                cout << "Enter only a file to decompress";
+                return;
+            }
+        }
+        else{
+            cout << "Choose an option --compress/--decompress";
+            return;
+        }
+        return;
+    }
+};
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Interface(argc, argv);
 }
